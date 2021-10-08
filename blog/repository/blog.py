354fd5dba2ce63a510ddc47ad4c3d667e023c8ca
Leestan360 <stanleymayore2@gmail.com>
2021-10-08 17:33:17 +0300

    commit all

--- blog/repository/blog.py
+++ blog/repository/blog.py
@@ -1,32 +1,22 @@
-from fastapi import FastAPI, Depends, status, Response, HTTPException
-from . import schemas, models
-from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
-
-app = FastAPI()
-
-
-models.Base.metadata.create_all(engine)
+from .. import models, schemas
+from fastapi import HTTPException, status
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def get_all(db: Session):
+    blogs = db.query(models.blog).all()
+    return blogs
 
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
+def create(request: schemas.Blog, db: Session):
+    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)   
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id,db: Session = Depends(get_db)):
+
+def destroy(id: int, db: Session):
     db.query(models.Blog).filter(models.Blog.id == id)
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -37,8 +27,8 @@ def destroy(id,db: Session = Depends(get_db)):
     db.commit()
     return 'done'
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+
+def update(id: int, request: schemas.Blog, db: Session):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -47,13 +37,8 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     return 'updated'
 
-@app.get('/blog')
-def all(db: Session = Depends(get_db)):
-    blogs = db.query(models.blog).all()
-    return blogs
 
-@app.get('/blog/{id}', status_code=200)
-def show(id, db: Session = Depends(get_db)):
+def show(id: int, db: Session):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,39 @@
+from typing import List
 from pydantic import BaseModel
 
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
+    
+    
+class Blog(BlogBase):   
+    class Config():
+        orm_mode = True
+
+
+class User(BaseModel):
+    name:str
+    email:str
+    password:str
+
+
+class ShowUser(BaseModel):
+    name:str
+    email:str
+    blog: List[Blog] = []
+    class Config():
+        orm_mode = True
+
+
+class ShowBlog(Blog):
+    title = str
+    body = str
+    creator: ShowUser
+    class Config():
+        orm_mode = True
+
+
+class Login(BaseModel):
+    username: str
+    password: str
